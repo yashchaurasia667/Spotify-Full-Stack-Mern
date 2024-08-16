f8f4a9c8e76a89d5d9f4372416a9358652f29427
Yash <yashchaurasia630@gmail.com>
2024-08-16 19:33:13 +0530

    started homepage frontend

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,4 +1,4 @@
-import Login from "./pages/login";
+import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 import Home from "./pages/Home";
 
--- FrontEnd/src/pages/Home.jsx
+++ FrontEnd/src/pages/Home.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
+import {Link} from 'react-router-dom'
+import spotify from "/spotifyBw.svg";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="bg-[#121212] text-white h-screen">
+      {/* Menu */}
+      <div>
+        <Link to="/" className="flex items-center">
+          <img src={spotify} className="scale-50" />
+          Spotify
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
