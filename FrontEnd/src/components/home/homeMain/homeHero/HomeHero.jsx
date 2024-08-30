0f67e14d33a5873f94c6c43c4b4be3a3f1e69c27
Yash <yashchaurasia630@gmail.com>
2024-08-30 21:32:00 +0530

    artist card done

--- FrontEnd/src/components/home/homeMain/homeHero/HomeHero.jsx
+++ FrontEnd/src/components/home/homeMain/homeHero/HomeHero.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import ArtistCard from "./ArtistCard";
 
-import { home_hero } from "../homeMain.module.css";
+import { home_hero, heading } from "../homeMain.module.css";
 
 const HomeHero = () => {
   return (
     <div className={`${home_hero}`}>
+      <div>
+        <button className={`${heading}`}>Popular artists</button>
+      </div>
       <ArtistCard name="Pritam" type="Artist" img="/pritam.jpeg" />
     </div>
   );
