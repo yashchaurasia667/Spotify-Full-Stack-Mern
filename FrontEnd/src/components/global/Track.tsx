5def014ece166ee7e1659df7d682c88578d66ebb
YASH <yashchaurasia630@gmail.com>
2024-12-06 21:44:57 +0530

    profile update

---
+++ FrontEnd/src/components/global/Track.tsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import { FaPlay } from "react-icons/fa";
+
+interface trackProps {
+  rank: number;
+  name: string;
+  artist: string;
+  cover: string;
+  album: string;
+  duration: string;
+}
+
+const Track = ({ rank, name, artist, cover, album, duration }: trackProps) => {
+  const [hover, setHover] = useState(false);
+  return (
+    <div
+      onMouseOver={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+      className="group grid grid-cols-[0.5fr_10fr_6fr_1fr] items-center px-4 py-2 rounded-md hover:bg-background-elevated-base"
+    >
+      <p className="text-text-subdued">
+        {hover ? <FaPlay fill="#acacac" /> : rank}
+      </p>
+
+      <div className="flex items-center gap-x-3">
+        <img src={cover} width={45} height={45} className="rounded-md" />
+        <div>
+          <p className="text-white">{name}</p>
+          <p className="text-sm text-text-subdued hover:underline hover:text-white">
+            {artist}
+          </p>
+        </div>
+      </div>
+
+      <p className="text-text-subdued">{album}</p>
+
+      <div className="flex gap-x-4">
+        <p className="text-text-subdued">{duration}</p>
+        <button className="text-text-subdued invisible -hover:visible">
+          •••
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Track;
--- FrontEnd/src/pages/Profile.tsx
+++ FrontEnd/src/pages/Profile.tsx
@@ -3,6 +3,7 @@ import ProfileHeader from "../components/profile/ProfileHeader";
 
 import ArtistCard from "../components/global/ArtistCard";
 import ArtistBar from "../components/global/ArtistBar";
+import Track from "../components/global/Track";
 
 import MainContext from "../context/mainContext/MainContext";
 
@@ -45,6 +46,31 @@ const Profile = () => {
               type="Artist"
               img="artists/CAS.jpg"
               className="w-[200px]"
+            />
+          </div>
+        </div>
+        <div>
+          <ArtistBar
+            main={
+              <>
+                <p className="text-2xl font-bold hover:underline">
+                  Top tracks this month
+                </p>
+                <p className="text-sm text-text-subdued font-medium hover:no-underline">
+                  Only visible to you
+                </p>
+              </>
+            }
+            side={"Show all"}
+          />
+          <div className="mt-3">
+            <Track
+              rank={1}
+              name="Lovers Rock"
+              artist="TV Girl"
+              cover="https://i.scdn.co/image/ab67616d00004851e1bc1af856b42dd7fdba9f84"
+              album="French Exit"
+              duration="3:33"
             />
           </div>
         </div>
