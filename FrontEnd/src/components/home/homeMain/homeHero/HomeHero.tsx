31b276247f871694d770e2302f3ef0732542cbb6
YASH <yashchaurasia630@gmail.com>
2024-12-19 21:18:28 +0530

    layout update

--- FrontEnd/src/components/home/homeMain/homeHero/HomeHero.tsx
+++ FrontEnd/src/components/home/homeMain/homeHero/HomeHero.tsx
@@ -10,6 +10,8 @@ const HomeHero = () => {
     <div className={`${styles.home_hero}`}>
       <div className="grid grid-cols-4 grid-rows-2 gap-y-4 gap-x-4 mt-5 overflow-hidden">
         <RecentlyPlayed cover="playlists/likedSongs.jpg" title="Liked Songs" />
+        <RecentlyPlayed cover="playlists/likedSongs.jpg" title="Liked Songs" />
+        <RecentlyPlayed cover="playlists/likedSongs.jpg" title="Liked Songs" />
       </div>
       <div>
         <ArtistBar
