5bb47190e469fbdd3d3d18ad131b8ff1549f046c
YASH <yashchaurasia630@gmail.com>
2024-12-21 23:15:57 +0530

    playlist page update

--- FrontEnd/src/pages/Playlist.tsx
+++ FrontEnd/src/pages/Playlist.tsx
@@ -1,23 +1,37 @@
 const Playlist = () => {
   return (
-    <div className="flex ">
+    <div className="relative">
+      <div className="z-0 h-1/2 w-full absolute bg-gradient-to-t from-[#00000066] to-[#00000000]"></div>
+      {/* <div className="bg-[#4b3692] h-full flex items-center text-7xl font-bold">
+        <div className="z-10">
+          <p>something</p>
+        </div>
+      </div> */}
       <div>
-        <img src="/public/playlists/likedSongs.jpg" width={250} height={250} />
-      </div>
-      <div>
-        <p>Playlist</p>
-        <h2>Liked Songs</h2>
-        <div>
+        <div className="flex items-center bg-[#4b3692] h-1/2 z-[1]">
           <div>
             <img
               src="/public/playlists/likedSongs.jpg"
-              width={30}
-              height={30}
-              className="rounded-[50%]"
+              width={250}
+              height={250}
             />
-            <p>Yash</p>
           </div>
-          <p>784 songs</p>
+          <div>
+            <p>Playlist</p>
+            <h2 className="text-8xl font-bold">Liked Songs</h2>
+            <div>
+              <div>
+                <img
+                  src="/public/playlists/likedSongs.jpg"
+                  width={30}
+                  height={30}
+                  className="rounded-[50%]"
+                />
+                <p>Yash</p>
+              </div>
+              <p>784 songs</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
