40f7bff9e6f02d6f1e2ba18f7695003dff63b37f
YASH <yashchaurasia630@gmail.com>
2024-12-22 22:59:34 +0530

    something

--- FrontEnd/src/pages/Playlist.tsx
+++ FrontEnd/src/pages/Playlist.tsx
@@ -2,11 +2,6 @@ const Playlist = () => {
   return (
     <div className="relative">
       <div className="z-0 h-1/2 w-full absolute bg-gradient-to-t from-[#00000066] to-[#00000000]"></div>
-      {/* <div className="bg-[#4b3692] h-full flex items-center text-7xl font-bold">
-        <div className="z-10">
-          <p>something</p>
-        </div>
-      </div> */}
       <div>
         <div className="flex items-center bg-[#4b3692] h-1/2 z-[1]">
           <div>
