b11b407382a7afbc1ddf428e042fc50b22c41e0b
YASH <yashchaurasia630@gmail.com>
2024-12-24 19:21:15 +0530

    banner done

--- FrontEnd/src/pages/Playlist.tsx
+++ FrontEnd/src/pages/Playlist.tsx
@@ -1,41 +1,31 @@
 const Playlist = () => {
   return (
-    // <div className="relative">
-    //   <div className="z-0 h-1/2 w-full absolute bg-gradient-to-t from-[#00000066] to-[#00000000]"></div>
-    //   <div className="px-4">
-    //     <div className="flex items-center bg-[#4b3692] h-1/2 z-[1]">
-    //       <div>
-    //         <img
-    //           src="/public/playlists/likedSongs.jpg"
-    //           width={250}
-    //           height={250}
-    //         />
-    //       </div>
-    //       <div>
-    //         <p>Playlist</p>
-    //         <h2 className="text-8xl font-bold">Liked Songs</h2>
-    //         <div>
-    //           <div>
-    //             <img
-    //               src="/public/playlists/likedSongs.jpg"
-    //               width={30}
-    //               height={30}
-    //               className="rounded-[50%]"
-    //             />
-    //             <p>Yash</p>
-    //           </div>
-    //           <p>784 songs</p>
-    //         </div>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </div>
-    <div className="relative bg-[#4b3692] h-1/2 bg-gradient-to-t from-[#00000066] to-[#00000000] flex items-center">
-      <div className="flex px-4">
-        <img src="/public/playlists/likedSongs.jpg" width={250} height={250} />
-        <div>
-          <p>Playlist</p>
-          <h2 className="text-8xl font-bold">Liked Songs</h2>
+    <div className="relative bg-[#4b3692] h-fit bg-gradient-to-t from-[#00000066] to-[#00000000]">
+      <div className="flex gap-x-5 items-center px-4 py-5 h-full">
+        <img
+          src="/public/playlists/likedSongs.jpg"
+          width={250}
+          height={250}
+          className="rounded-md shadow-[0px_0px_20px_#00000088]"
+        />
+        <div className="flex flex-col gap-y-3">
+          <p className="font-medium">Playlist</p>
+          <h2 className="text-8xl font-extrabold -tracking-[0.2rem]">
+            Liked Songs
+          </h2>
+          <div className="flex gap-x-3 items-center">
+            <div
+              style={{
+                width: 25,
+                height: 25,
+              }}
+            >
+              <img src="/public/radio.png" className="rounded-[50%]" />
+            </div>
+            <p className="text-text-subdued -tracking-wider font-medium">
+              768 Songs
+            </p>
+          </div>
         </div>
       </div>
     </div>
