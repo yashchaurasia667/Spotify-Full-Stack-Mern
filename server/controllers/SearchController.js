a37a38d2704b127edbf7ef6d8c63f45354b2b839
YASH <yashchaurasia630@gmail.com>
2025-01-03 12:27:14 +0530

    authorization code flow

--- server/controllers/SearchController.js
+++ server/controllers/SearchController.js
@@ -1,3 +1,6 @@
+import bcrypt from "bcryptjs";
+import querystring from "querystring";
+
 export const getToken = async (req, res) => {
   const body = new URLSearchParams({
     grant_type: "client_credentials",
@@ -25,5 +28,37 @@ export const getToken = async (req, res) => {
 }
 
 export const login = async (req, res) => {
-  const state = generateRandomString(16);
+  const state = bcrypt.genSaltSync(16);
+  const scope = "user-read-playback-state user-read-currently-playing playlist-read-private playlist-read-collaborative user-follow-read user-library-read user-read-email";
+
+  res.redirect("https://accounts.spotify.com/authorize?" + querystring.stringify({
+    response_type: "code",
+    client_id: process.env.API_ID,
+    scope: scope,
+    redirect_uri: "http://localhost:4000/search/callback",
+    state: state
+  }));
+}
+
+export const callback = async (req, res) => {
+  const code = req.query.code || null;
+  const state = req.query.state || null;
+
+  if (state) {
+    const authOptions = {
+      url: "https://accounts.spotify.com/api/token",
+      form: {
+        code: code,
+        redirect_uri: "http://localhost:4000/search/callback",
+        grant_type: "authorization_code"
+      },
+      headers: {
+        "content-type": "application/x-www-form-urlencoded",
+        "Authorization": "Basic " + (Buffer.from(process.env.API_ID + ":" + process.env.API_SECRET).toString("base64"))
+      },
+      json: true
+    };
+  }
+  else
+    res.redirect("/#" + querystring.stringify({ error: "state mismatch" }));
 }
--- server/routes/SearchRouter.js
+++ server/routes/SearchRouter.js
@@ -1,9 +1,11 @@
 import express from "express";
 
-import { getToken } from "../controllers/SearchController.js";
+import { callback, getToken, login } from "../controllers/SearchController.js";
 
 const router = express.Router();
 
 router.get("/gettoken", getToken);
+router.get("/login", login);
+router.get("/callback", callback);
 
 export default router;
